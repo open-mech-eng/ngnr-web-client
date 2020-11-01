cdcbe552cbb21fb3cfdfecbb222cc3ac25e748c3
noce2 <20339399+noce2@users.noreply.github.com>
2020-11-01 23:21:38 +0000

    Enable setting of alerts as variables

--- src/app/explore/explore-app-alert/explore-app-alert.component.ts
+++ src/app/explore/explore-app-alert/explore-app-alert.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
+export interface AlertMessage {
+  message: string;
+  importance: 'info' | 'warning' | 'danger';
+}
 @Component({
   selector: 'app-explore-app-alert',
   templateUrl: './explore-app-alert.component.html',
-  styleUrls: ['./explore-app-alert.component.scss']
+  styleUrls: ['./explore-app-alert.component.scss'],
 })
 export class ExploreAppAlertComponent implements OnInit {
+  private privateAlertMessages$ = new BehaviorSubject<AlertMessage[]>([]);
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  get alertMessages$(): BehaviorSubject<AlertMessage[]> {
+    return this.privateAlertMessages$;
   }
-
 }
