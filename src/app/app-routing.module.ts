33b02f36bdced5d4baa9eedb4d3e10e100438bb5
noce2 <20339399+noce2@users.noreply.github.com>
2020-11-01 21:58:13 +0000

    Redirect root to home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { ExploreComponent } from './explore/explore.component';
 
 const routes: Routes = [
+  { path: 'explore', component: ExploreComponent },
   { path: 'home', component: ExploreComponent },
-  { path: '', component: ExploreComponent },
+  { path: '', redirectTo: 'home' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
