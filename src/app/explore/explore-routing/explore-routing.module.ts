e0a1e1471bbafcb35812c925889b07a1a98a7135
noce2 <20339399+noce2@users.noreply.github.com>
2020-11-01 22:34:54 +0000

    Setup dashboard route for explore home

--- src/app/explore/explore-routing/explore-routing.module.ts
+++ src/app/explore/explore-routing/explore-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ExploreDashboardComponent } from '../explore-dashboard/explore-dashboard.component';
 import { ExploreComponent } from '../explore.component';
 
-const routes: Routes = [{ path: '', component: ExploreComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: ExploreComponent,
+    children: [{ path: '', component: ExploreDashboardComponent }],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
